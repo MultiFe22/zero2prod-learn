8a0a1c054d5a7d5420a9c275105b3da35892faf3
Felipe Vidal <felipevidal@sinaitechnologies.com>
2023-04-19 07:59:59 -0300

    implemented health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world");
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    // Set listener to run on port 0, which will assign a random port.
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // Run the server.
+    run(listener)?.await
 }
