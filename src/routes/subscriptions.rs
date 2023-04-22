f6f9aa0ed4aae19cfb6dce4cfd966c9b7f6c583b
Felipe Vidal <felipevidal@sinaitechnologies.com>
2023-04-22 18:01:13 -0300

    minor fmt

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -8,7 +8,6 @@ pub struct FormData {
     name: String,
 }
 
-
 #[tracing::instrument(
     name = "Adding a new subscriber",
     skip(form, pool),
@@ -19,26 +18,17 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    match insert_subscriber(&pool, &form).await
-    {
-        Ok(_) => {
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => {
-            HttpResponse::InternalServerError().finish()
-        }
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
 #[tracing::instrument(
-    name = "Saving new subscriber details in the database", 
+    name = "Saving new subscriber details in the database",
     skip(form, pool)
 )]
-
-pub async fn insert_subscriber(
-    pool: &PgPool,
-    form: &FormData,
-) -> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -50,7 +40,7 @@ pub async fn insert_subscriber(
     )
     .execute(pool)
     .await
-    .map_err( |e| {
+    .map_err(|e| {
         tracing::error!("Failed to execute query: {:?}", e);
         e
     })?;
