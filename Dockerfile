b8cba89f4059dadec4bfa0a2e53092c2fafa0daa
Felipe Vidal <felipevidal@sinaitechnologies.com>
2023-05-01 15:30:10 -0300

    Adding yamls and DB

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+FROM lukemathwalker/cargo-chef:latest as chef
+WORKDIR /app
+RUN apt update && apt install lld clang -y
+
+FROM chef as planner
+# Copy all files from our working environment to our Docker image
+COPY . .
+# Compute a lock-like file for our project
+RUN cargo chef prepare  --recipe-path recipe.json
+
+FROM chef as builder
+COPY --from=planner /app/recipe.json recipe.json
+RUN cargo chef cook --release --recipe-path recipe.json
+COPY . .
+ENV SQLX_OFFLINE true
+
+# Let's build our binary!
+# We'll use the release profile to make it faaaast
+RUN cargo build --release --bin zero2prod
+
+FROM debian:bullseye-slim AS runtime
+WORKDIR /app
+RUN apt-get update -y \
+    && apt-get install -y --no-install-recommends openssl ca-certificates \
+    # Clean up
+    && apt-get autoremove -y \
+    && apt-get clean -y \
+    && rm -rf /var/lib/apt/lists/*
+COPY --from=builder /app/target/release/zero2prod zero2prod
+COPY configuration configuration
+ENV APP_ENVIRONMENT production
+ENTRYPOINT ["./zero2prod"]
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use zero2prod::{configuration::get_configuration, startup::run};
 
@@ -11,12 +11,14 @@ async fn main() -> Result<(), std::io::Error> {
     // Panic if we can't read the configuration.
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    let connection_pool = PgPool::connect(&configuration.database.connection_string().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool =
+        PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+            .expect("Failed to connect to Postgres connection pool.");
 
     // Now the port is read from the configuration file.
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host ,configuration.application.port);
     // print the address to the console.
     println!("Listening on http://{}", address);
     // Set listener to run on port 0, which will assign a random port.
